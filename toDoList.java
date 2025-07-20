4c1d2dcb9cbdb530aa5cc098ba0679e4004e9dca
Charan K S <121172957+ghoul1130@users.noreply.github.com>
2025-07-20 20:10:54 +0530

    Add files via upload

---
+++ toDoList.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class toDoList {
+    String name;
+    Boolean isCompleted;  // Remove "static" - each task needs its own status
+    static ArrayList<toDoList> taskList = new ArrayList<>();
+
+    public static void addToList(toDoList task) {
+        long startTime = System.nanoTime();
+
+        taskList.add(task);
+        System.out.println(task.name + " added to the list");
+
+        long endTime = System.nanoTime();
+        System.out.println("Execution time: " + (endTime - startTime) + " ns");
+    }
+
+    public static void markAsCompleted(toDoList task) {
+        long startTime = System.nanoTime();
+
+        task.isCompleted = true;  // Set the specific task's status
+        System.out.println(task.name + " marked as completed");
+
+        long endTime = System.nanoTime();
+        System.out.println("Execution time: " + (endTime - startTime) + " ns");
+    }
+
+    public static void removeFromList(toDoList task) {
+        long startTime = System.nanoTime();
+
+        taskList.remove(task);
+        System.out.println(task.name + " removed from list");
+
+        long endTime = System.nanoTime();
+        System.out.println("Execution time: " + (endTime - startTime) + " ns");
+    }
+
+    public static void DisplayAll() {
+        long startTime = System.nanoTime();
+
+        System.out.println("All Tasks:");
+        for (toDoList task : taskList) {
+            String status = task.isCompleted ? "Done" : "Not Done";
+            System.out.println(task.name + " :: " + status);
+        }
+
+        long endTime = System.nanoTime();
+        System.out.println("Execution time: " + (endTime - startTime) + " ns");
+    }
+
+    public static void main(String[] args) {
+        toDoList task1 = new toDoList();
+        task1.name = "Wake up";
+        task1.isCompleted = false;
+
+        toDoList task2 = new toDoList();
+        task2.name = "Read a book";
+        task2.isCompleted = false;
+
+        toDoList task3 = new toDoList();
+        task3.name = "Play football";
+        task3.isCompleted = false;
+
+        addToList(task1);
+        addToList(task2);
+        addToList(task3);
+
+        markAsCompleted(task1);
+        markAsCompleted(task2);
+        removeFromList(task1);
+
+        DisplayAll();
+    }
+}
