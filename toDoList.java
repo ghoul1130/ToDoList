0fc3aa0f8ff6a85f5539b1d453121d48c370620f
Charan K S <121172957+ghoul1130@users.noreply.github.com>
2025-07-22 11:31:37 +0530

    Update toDoList.java

--- toDoList.java
+++ toDoList.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
-public class toDoList {
+class tracker {
     String name;
-    Boolean isCompleted;  
-    static ArrayList<toDoList> taskList = new ArrayList<>();
+    Boolean isCompleted;
+    static ArrayList<tracker> taskList = new ArrayList<>();
 
-    public static void addToList(toDoList task) {
+    public static void addToList(tracker task) {
         long startTime = System.nanoTime();
 
         taskList.add(task);
@@ -15,17 +15,17 @@ public class toDoList {
         System.out.println("Execution time: " + (endTime - startTime) + " ns");
     }
 
-    public static void markAsCompleted(toDoList task) {
+    public static void markAsCompleted(tracker task) {
         long startTime = System.nanoTime();
 
-        task.isCompleted = true;  // Set the specific task's status
+        task.isCompleted = true;
         System.out.println(task.name + " marked as completed");
 
         long endTime = System.nanoTime();
         System.out.println("Execution time: " + (endTime - startTime) + " ns");
     }
 
-    public static void removeFromList(toDoList task) {
+    public static void removeFromList(tracker task) {
         long startTime = System.nanoTime();
 
         taskList.remove(task);
@@ -39,7 +39,7 @@ public class toDoList {
         long startTime = System.nanoTime();
 
         System.out.println("All Tasks:");
-        for (toDoList task : taskList) {
+        for (tracker task : taskList) { 
             String status = task.isCompleted ? "Done" : "Not Done";
             System.out.println(task.name + " :: " + status);
         }
@@ -47,46 +47,48 @@ public class toDoList {
         long endTime = System.nanoTime();
         System.out.println("Execution time: " + (endTime - startTime) + " ns");
     }
+}
 
+public class toDoList {
     public static void main(String[] args) {
-        toDoList task1 = new toDoList();
+        tracker task1 = new tracker();
         task1.name = "Wake up";
         task1.isCompleted = false;
 
-        toDoList task2 = new toDoList();
+        tracker task2 = new tracker();
         task2.name = "Read a book";
         task2.isCompleted = false;
 
-        toDoList task3 = new toDoList();
+        tracker task3 = new tracker();
         task3.name = "Play football";
         task3.isCompleted = false;
 
-        addToList(task1);
-        addToList(task2);
-        addToList(task3);
+        tracker.addToList(task1);
+        tracker.addToList(task2);
+        tracker.addToList(task3);
 
-        markAsCompleted(task1);
-        markAsCompleted(task2);
-        removeFromList(task1);
+        tracker.markAsCompleted(task1);
+        tracker.markAsCompleted(task2);
+        tracker.removeFromList(task1);
 
-        DisplayAll();
+        tracker.DisplayAll();
     }
 }
 
-//OUTPUT
-//Wake up added to the list
-//Execution time: 957900 ns
-//Read a book added to the list
-//Execution time: 13800 ns
-//Play football added to the list
-//Execution time: 9200 ns
-//Wake up marked as completed
-//Execution time: 39900 ns
-//Read a book marked as completed
-//Execution time: 11700 ns
-//Wake up removed from list
-//Execution time: 35700 ns
-//All Tasks:
-//Read a book :: Done
-//Play football :: Not Done
-//Execution time: 5634300 ns
+OUTPUT
+Wake up added to the list
+Execution time: 1085600 ns
+Read a book added to the list
+Execution time: 15400 ns
+Play football added to the list
+Execution time: 10000 ns
+Wake up marked as completed
+Execution time: 50600 ns
+Read a book marked as completed
+Execution time: 12600 ns
+Wake up removed from list
+Execution time: 37800 ns
+All Tasks:
+Read a book :: Done
+Play football :: Not Done
+Execution time: 4223500 ns
